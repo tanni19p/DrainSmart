8b163924c38cd3b615f4f2c213439b7ff08a020c
Khushi Roy <khushinishu2005@gmail.com>
2025-12-31 21:35:48 +0530

    chore: fix CORS and ignore vite cache

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+
 const wardRoutes = require("./routes/ward.routes");
 const hotspotRoutes = require("./routes/hotspot.routes");
 const alertRoutes = require("./routes/alert.routes");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-app.use("/api/wards", wardRoutes);
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/wards", wardRoutes);
 app.use("/api/hotspots", hotspotRoutes);
-
 app.use("/api/alerts", alertRoutes);
 
 app.get("/", (req, res) => {
@@ -22,3 +25,4 @@ app.get("/", (req, res) => {
 });
 
 module.exports = app;
+
